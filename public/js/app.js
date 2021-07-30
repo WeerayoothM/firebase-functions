82fa53aba573f99ca719cd872ade393acd4ea05e
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-07-30 23:12:21 +0700

    [Added] Adding Requests (client-side)

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 const requestModal = document.querySelector(".new-request");
 const requestLink = document.querySelector(".add-request");
+const requestForm = document.querySelector(".new-request form");
 
 // open request modal
 requestLink.addEventListener("click", () => {
@@ -13,11 +14,29 @@ requestModal.addEventListener("click", (e) => {
   }
 });
 // say hello function call
-const button = document.querySelector(".call");
-button.addEventListener("click", () => {
-  // get function reference
-  const sayHello = firebase.functions().httpsCallable("sayHello");
-  sayHello({ name: "Weerayooth" }).then((result) => {
-    console.log(result.data);
-  });
+// const button = document.querySelector(".call");
+// button.addEventListener("click", () => {
+//   // get function reference
+//   const sayHello = firebase.functions().httpsCallable("sayHello");
+//   sayHello({ name: "Weerayooth" }).then((result) => {
+//     console.log(result.data);
+//   });
+// });
+
+// add a new request
+requestForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const addRequest = firebase.functions().httpsCallable("addRequest");
+  addRequest({
+    text: requestForm.request.value,
+  })
+    .then(() => {
+      requestForm.reset();
+      requestModal.classList.remove("open");
+      requestForm.querySelector(".error").textContent = "";
+    })
+    .catch((err) => {
+      requestForm.querySelector(".error").textContent = err.message;
+    });
 });
