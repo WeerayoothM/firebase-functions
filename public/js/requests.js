8a778fb41ddc491510bda4fbc00bace57bbac673
Weerayooth Manawanich <weerayooth.ma@gmail.com>
2021-07-31 23:03:31 +0700

    [Added] Adding a Vue Instance

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,16 +1,19 @@
-const ref = firebase.firestore().collection("requests");
+var app = new Vue({
+  el: "#app",
+  data: {
+    requests: [],
+  },
+  mounted() {
+    const ref = firebase.firestore().collection("requests");
 
-// callback function everytime a change in this collection occurs
-ref.onSnapshot((snapshot) => {
-  let requests = [];
-  snapshot.forEach((doc) => {
-    requests.push({ ...doc.data(), id: doc.id });
-  });
+    // callback function everytime a change in this collection occurs
+    ref.onSnapshot((snapshot) => {
+      let requests = [];
+      snapshot.forEach((doc) => {
+        requests.push({ ...doc.data(), id: doc.id });
+      });
 
-  let html = ``;
-  requests.forEach((request) => {
-    html += `<li>${request.text}</li>`;
-  });
-
-  document.querySelector("ul").innerHTML = html;
+      this.requests = requests;
+    });
+  },
 });
